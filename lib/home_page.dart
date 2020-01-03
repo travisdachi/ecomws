75ad1e61a5379c362efd5c52712fa797ff3a68e5
travis <travis.suban@gmail.com>
2020-01-04 01:55:33 +0700

    step 2 create model + call api function

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:ecomws/models.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatelessWidget {
   @override
@@ -35,4 +39,11 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+}
+
+Future<List<Product>> getProducts() async {
+  final response = await http.get('http://www.mocky.io/v2/5e0f46473400000d002d7fd0').then((x) => x.body);
+  final list = jsonDecode(response) as List;
+  final products = list.map((x) => Product.fromJson(x)).toList();
+  return products;
 }
---
+++ lib/models.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/foundation.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'models.g.dart';
+
+/// run command `flutter packages run build_runner build --delete-conflicting-outputs` to generate implementation
+@JsonSerializable()
+class Product {
+  final String id;
+  final String name;
+  final String brand;
+  final String os;
+  final String imageUrl;
+  final int price;
+
+  factory Product.fromJson(Map<String, dynamic> json) => _$ProductFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ProductToJson(this);
+
+  const Product({
+    @required this.id,
+    @required this.name,
+    @required this.brand,
+    @required this.os,
+    @required this.imageUrl,
+    @required this.price,
+  });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Product &&
+          runtimeType == other.runtimeType &&
+          id == other.id &&
+          name == other.name &&
+          brand == other.brand &&
+          os == other.os &&
+          imageUrl == other.imageUrl &&
+          price == other.price;
+
+  @override
+  int get hashCode => id.hashCode ^ name.hashCode ^ brand.hashCode ^ os.hashCode ^ imageUrl.hashCode ^ price.hashCode;
+}
